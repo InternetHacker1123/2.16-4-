40f02fc05a9cb4933a02037deec3fc5a2916ef05
internetHacker1123 <dinozavrik9911@mail.ru>
2024-04-22 23:15:51 +0300

    add examples

---
+++ pyCharm/examples/json2.py
@@ -0,0 +1,7 @@
+{
+    "students": [
+        {"firstName": "Tom", "lastName": "Jackson"},
+        {"firstName": "Linda", "lastName": "Garner"},
+        {"firstName": "Adam", "lastName": "Cooper"}
+    ]
+}
